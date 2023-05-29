dba17ffe10d27be73207170035e04a90e75a2074
Junior de Jesus <junior-de-jesus@live.com>
2023-05-29 20:51:22 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+// Variáveis
+const screen1 = document.querySelector('.screen1');
+const screen2 = document.querySelector('.screen2');
+const btn1 = document.querySelector('.btn1');
+const img1 = document.querySelector(".img1");
+
+
+
+img1.addEventListener('click', function()
+{
+    screen1.classList.toggle("hide")
+    screen2.classList.toggle("hide")
+})
+
+btn1.addEventListener('click', function(){
+screen1.classList.toggle("hide")
+screen2.classList.toggle("hide")
+})
